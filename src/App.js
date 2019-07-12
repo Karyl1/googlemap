78fadc090046b5d6d39dcf7cd896582e73565087
Karyl1 <karylkenth@gmail.com>
2019-07-12 17:13:47 +0200

    Création d'une branche backup

--- src/App.js
+++ src/App.js
@@ -15,11 +15,6 @@ class App extends Component {
                 <MapContainer className="MAP"/>
 
             </div>
-                {/* <ul className="githubFooter">
-                  <li><a href="https://github.com/Karyl1"><img className="githubLogo" src="https://avatars3.githubusercontent.com/u/45764416?s=460&amp;v=4"/></a></li>
-                  <li><a href="https://github.com/ArnaudMoya"><img className="githubLogo" src="https://avatars3.githubusercontent.com/u/44874070?s=460&amp;v=4"/></a></li>
-                  <li><a href="https://github.com/Ciszowski"><img className="githubLogo" src="https://avatars3.githubusercontent.com/u/45790380?s=460&amp;v=4"/></a></li>
-                </ul> */}
                 <a className="scroll_down" href="#scroll_down"><span role="img" aria-label="Arrow down">⏬</span></a>
                 <a className="scroll_up" href="#scroll_up"><span role="img" aria-label="Arrow up">⏫</span></a>
             </div>
